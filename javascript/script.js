2dbb217fff8f1c567837654a0bb86afe396e9663
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-10-05 12:07:47 +0530

    day1

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+let hobbies = ' coding reading runnning'
+let result =hobbies .trim()// removes empty spaces
+
+lastIndexOf ("running") //case sensitive indexing
+includes // to check whether word is present in the sentence
+slice(0,5) // exclude the last one 
+//strings are immutable
+
+// using priority and precedence
+/*1()Brakets 
+2* **power
+3 * / %(from left to right)
+4 + - (from left to right)
+
+let resulting = " My total score is = +result;
+dynamicaly typed langauage 
+
+Loose Equality Operator ==
+let age = 22;
+console.log (age == "22")
+
+Strict Equality operator
+console.log (age === "22")
+
+
+
+########Type conversion
+let stringType ="54";
+console.log (stringType , typeof stringType  )
+
+console.log 
+//checking  for boolean let age  ="";//empty string is true
+
+
+
+*/
---
+++ javascript/splitfunction.js
@@ -0,0 +1,3 @@
+let favoritecolor = "Brown Blue Blac Gray";
+let arrColors = favoritecolor.split(' ')
+console.log (arrColors)
