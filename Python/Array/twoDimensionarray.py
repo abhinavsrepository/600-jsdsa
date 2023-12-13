1c4c1a64a0ea408944a2ad800ae8b85cb3161da2
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-13 23:40:27 +0530

    helllo moto

--- Python/Array/twoDimensionarray.py
+++ Python/Array/twoDimensionarray.py
@@ -1,2 +1,5 @@
 import numpy as np
-twoDArray = np.array([[]])
+twoDArray = np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
+print(twoDArray)
+newTwoDArray = np.insert(twoDArray,0 ,[[1,2,3,4]],axis=1)
+print(newTwoDArray)
